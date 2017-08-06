752bb616bdcea544461efbc5900fdbed473b1ba1
Harkiratsingh026 <harkiratsingh.026@gmail.com>
2017-08-06 20:01:00 +0530

    DFS Binary Search Tree Commit .

---
+++ DFS_Binary_Search_Tree.py
@@ -0,0 +1,76 @@
+class node:
+    def __init__(self,data):
+        self.value=data;
+        self.count=0;
+        self.left=None;
+        self.right=None;
+
+class Tree:
+    def __init__(self):
+        self.head=None;
+
+    def add(self,data):
+        newnode=node(data);
+        hd=self.head;
+        if(self.head is None):
+            self.head=newnode;
+            self.head.count+=1;
+            return;
+        while(True):
+            if(data<hd.value):
+                if(hd.left is not None):
+                    hd=hd.left;
+                else:
+                    hd.left=newnode;
+                    hd.left.count=1;
+                    return;
+            elif(hd.value==data):
+                hd.count+=1;
+                return;
+            else:
+                if(hd.right is not None):
+                    hd=hd.right;
+                else:
+                    hd.right=newnode;
+                    hd.right.count=1;
+                    return;
+                    
+    def preorder(self,node):
+        if(node is None):
+            return;
+        print(node.value,node.count);
+        Tree.preorder(self,node.left);
+        Tree.preorder(self,node.right);
+
+    def inorder(self,node):
+        if(node is None):
+            return;
+        Tree.inorder(self,node.left);
+        print(node.value,node.count);
+        Tree.inorder(self,node.right);
+
+    def postorder(self,node):
+        if(node is None):
+            return;
+        Tree.postorder(self,node.left);
+        Tree.postorder(self,node.right);
+        print(node.value,node.count);
+            
+                
+
+if __name__=='__main__':
+    bst=Tree();
+    bst.add(5);
+    bst.add(3);
+    bst.add(4);
+    bst.add(2);
+    bst.add(10);
+    bst.add(10);
+    bst.add(5);
+    print("Pre-Order Traversal:");
+    bst.preorder(bst.head);
+    print("In-Order Traversal:");
+    bst.inorder(bst.head);
+    print("Post-Order Traversal:");
+    bst.postorder(bst.head);
+    
